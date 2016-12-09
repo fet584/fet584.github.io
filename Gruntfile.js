00644202e5659868637629cdcc308b4b74404eb7
fet584 <fet584@163.com>
2016-12-09 21:20:33 +0800

    optimized css files.
    inline style.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,32 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
     grunt.initConfig({
         uglify:{
             my_target:{
                 files:{
-                    'dest/main.min.js':['views/js/main.js']
+                    'dest/main.min.js':['views/js/main.js'],
                 }
             }
         },
+
+        cssmin:{
+            target:{
+                files:{
+                    'views/css/bootstrap-grid.min.css':['views/css/bootstrap-grid.css'],
+                    'css/style.css':['css/style.min.css']
+                }
+            }
+        }
 
     });
 
 
     grunt.registerTask('default',[
         'uglify',
+        'cssmin'
         ]);
 };
---
+++ optimizeList.md
@@ -0,0 +1,27 @@
+# Optimize List
+
+### All
+
+1.Change the size of the images and compressed them.
+
+
+
+### Pizza.html
+
+1.Inline the style.css.
+
+2.Compress the bootstrap-grid.css to bootstrap.min.css
+
+3.Async the analytics.js
+
+
+
+### index.html
+
+1.Make the analytics async.
+
+2.Put the link of the font style to the bottom.
+
+3.Inline the styles.css
+
+4.Add mediaquery(print)  to print.css
